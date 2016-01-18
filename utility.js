c3af05eadedb7d68429e9ba68bf82eb9bfb47745
James <valeness84@gmail.com>
2016-01-18 09:40:24 -0700

    Update utility.js

--- utility.js
+++ utility.js
@@ -24,4 +24,11 @@ if (!String.prototype.format) {
     };
 }
 
+// ToggleText helper
+$.fn.toggleText = function(t1, t2){
+    if (this.text() == t1) this.text(t2);
+    else                   this.text(t1);
+    return this;
+};
+
 /** Jquery **/
